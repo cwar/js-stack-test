d4e39baa76b12e9bd1350eb078e2acb4a01ec381
Chris Warren <cwarren13@gmail.com>
2018-01-10 10:27:02 -0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// @flow
+/* eslint-disable no-console */
+
+import Dog from './dog'
+
+const shepard = new Dog('shepard')
+
+console.log(shepard.bark())
+
+// flow-disable-next-line
+// something.flow(doesnt.like).for.instance()
